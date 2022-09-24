e5cc58af8ff440f0e98f75c407b26810a48dcd49
MCorange99 <79301995+MCorange99@users.noreply.github.com>
2022-09-24 21:59:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,18 +21,18 @@ echo "Do you want to clone with ssh or https? You will need to be registered on 
 (ssh/https)?: "
 read s_git_type
 
-if [ "$s_git_type" = "ssh" ] 
-then
+#if [ "$s_git_type" = "ssh" ] 
+#then
+#    echo "[CMD]: git clone https://github.com/MCorange99/mcLang.git ."
+#    git clone https://github.com/MCorange99/mcLang.git .
+#elif [ "$s_git_type" = "https" ] 
+#then
     echo "[CMD]: git clone https://github.com/MCorange99/mcLang.git ."
     git clone https://github.com/MCorange99/mcLang.git .
-elif [ "$s_git_type" = "https" ] 
-then
-    echo "[CMD]: git clone https://github.com/MCorange99/mcLang.git ."
-    git clone https://github.com/MCorange99/mcLang.git .
-else
-    echo "Unknown Type $s_git_type."
-    exit
-fi
+#else
+#    echo "Unknown Type $s_git_type."
+#    exit
+#fi
 cp mclang.py mclang
 chmod +x mclang
 
